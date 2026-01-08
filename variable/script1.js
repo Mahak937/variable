1ce97a671df16ac82e99e6e3ddd4e8f500717ac2
Mahak Shrivastava <mahakshrivastava134@gmail.com>
2026-01-08 19:23:35 +0530

    Add files via upload

---
+++ variable/script1.js
@@ -0,0 +1,13 @@
+let name = "Mahak";
+let age = 21;
+let city = "Jabalpur";
+
+// console.log("Name = ",name,",","Type is ", typeof name);
+// console.log("Age = ",age, ",","Type is ", typeof age);
+// console.log("City = ",city, ",","Type is ", typeof city);
+
+// use template
+
+console.log(`Name: ${name} (Typeof: ${typeof name})`);
+console.log(`Age: ${age} (Typeof: ${typeof age})`);
+console.log(`City: ${city} (Typeof: ${typeof city})`);
